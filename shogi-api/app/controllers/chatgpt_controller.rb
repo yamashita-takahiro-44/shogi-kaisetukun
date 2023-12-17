0270401243673b08af2c69639cf48ef44a564976
yamashita-takahiro-44 <shimolen@gmail.com>
2023-12-18 06:13:35 +0900

    棋譜アップロード対応

--- shogi-api/app/controllers/chatgpt_controller.rb
+++ shogi-api/app/controllers/chatgpt_controller.rb
@@ -7,22 +7,30 @@ class ChatgptController < ApplicationController
     # パラメータの確認
     Rails.logger.info "Received params: #{params.inspect}"
     moves = params[:moves]
+    board = params[:board] # 追加されたboardパラメータ
 
-    uri = URI('https://api.openai.com/v1/engines/text-davinci-003/completions')
+    # GPTに送るプロンプトを決定
+    prompt = if board.present?
+               "あなたは将棋の解説者です。現在の盤面はSFEN形式で以下の通りです。\n#{board}\nこれに対して解説者っぽく解説をしてください。解説する内容は、現在の盤面の状況分析と次の手の予想です。回答は400字以内でお願いします。"
+             else
+               "あなたは将棋の解説者です。初期盤面からの将棋の手順は以下の通り。: #{moves}。これに対して解説者っぽく解説をしてください。解説する内容は、戦型と囲いの名前と最後の手の解説と次の手の予想です。回答は400字以内でお願いします。"
+             end
 
+    # APIリクエストの設定
+    uri = URI('https://api.openai.com/v1/completions')
     request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{ENV['OPENAI_API_KEY']}")
-
     request.body = {
-      prompt: "あなたは将棋の解説者です。初期盤面からの将棋の手順は以下の通り。: #{moves}。これに対して解説者っぽく解説をしてください。解説する内容は、戦型と囲いの名前と最後の手の解説と次の手の予想です。",
+      model: "gpt-3.5-turbo-instruct",
+      prompt: prompt,
       max_tokens: 400
     }.to_json
 
+    # APIリクエストの実行
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
     end
 
     Rails.logger.info "API Response: #{response.body}"
-
     render json: response.body
   end
 end
--- shogi-frontend/src/components/GameBoard.js
+++ shogi-frontend/src/components/GameBoard.js
@@ -9,40 +9,100 @@ const GameBoard = () => {
   const [moves, setMoves] = useState([]);
   const [imageURL, setImageURL] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [board, setBoard] = useState(null);
+  const [spMode, setSpMode] = useState("play");
 
   useEffect(() => {
     const shogiPlayer = shogiRef.current;
     if (!shogiPlayer) return;
 
     const handleMove = (e) => {
-      const newMove = e.detail[0].last_move_info.to_kif;
-      setMoves(prevMoves => [...prevMoves, newMove]);
+      if (spMode === "play") {
+        const newMove = e.detail[0].last_move_info.to_kif;
+        setMoves(prevMoves => [...prevMoves, newMove]);
+      }
     };
+
+    
+    const handleSfenChange = (e) => {
+      if (spMode !== "play") {
+        const sfen = e.detail[0];
+        console.log("SFEN:", sfen); // ログでSFENの値を確認
+    
+        if (!sfen) {
+          console.error("SFEN is undefined or null");
+          return;
+        }
+    
+        setBoard(sfen); // 直接SFEN文字列をboardステートにセット
+      }
+    };
+    
+
+    shogiPlayer.addEventListener('ev_play_mode_move', handleMove);
+    shogiPlayer.addEventListener('ev_short_sfen_change', handleSfenChange);
 
     const handleIllegalAccident = (e) => {
       const illegalDetails = e.detail[0];
       const illegalName = illegalDetails.name;
       alert('反則が発生しました: ' + illegalName);
     };
-    
 
-    shogiPlayer.addEventListener('ev_play_mode_move', handleMove);
     shogiPlayer.addEventListener('ev_illegal_illegal_accident', handleIllegalAccident);
 
     return () => {
       shogiPlayer.removeEventListener('ev_play_mode_move', handleMove);
+      shogiPlayer.removeEventListener('ev_short_sfen_change', handleSfenChange);
       shogiPlayer.removeEventListener('ev_illegal_illegal_accident', handleIllegalAccident);
     };
-  }, []);
+  }, [spMode]);
 
+  useEffect(() => {
+    if (shogiRef.current) {
+      shogiRef.current.setAttribute('sp_mode', spMode);
+      shogiRef.current.setAttribute('sp_turn', spMode === "play" ? "" : "-1");
+      shogiRef.current.setAttribute('sp_slider', spMode !== "play" ? "true" : "false");
+    }
+  }, [spMode, shogiRef]);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const fileContent = e.target.result;
+        setSpMode("replay"); // 再生モードを示す値に設定
+        if (shogiRef.current) {
+          shogiRef.current.setAttribute('sp_body', fileContent); // sp_body 属性を設定
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+  
+  useEffect(() => {
+    if (shogiRef.current) {
+      if (spMode === "replay") {
+        shogiRef.current.removeAttribute('sp_mode'); // 再生モードの場合は sp_mode 属性を削除
+        shogiRef.current.setAttribute('sp_slider', "true"); // スライダーを表示
+        shogiRef.current.setAttribute('sp_controller', "true"); // コントローラを表示
+      } else {
+        shogiRef.current.setAttribute('sp_mode', "play"); // 操作モードの場合は sp_mode を設定
+        shogiRef.current.setAttribute('sp_slider', "false"); // スライダーを非表示
+      }
+    }
+  }, [spMode, shogiRef]);
+  
   const handleExplain = () => {
     setIsLoading(true);
+  
+    let requestBody = board ? { board: board } : { moves: moves };
+  
+    console.log("Request body:", requestBody);
+  
     fetch('https://shogikaisetukun.fly.dev/chatgpt/explain', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ moves: moves })
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody)
     })
     .then(response => response.json())
     .then(data => {
@@ -72,10 +132,8 @@ const GameBoard = () => {
   const uploadImage = (dataURL) => {
     fetch('https://shogikaisetukun.fly.dev/images', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ image: dataURL }),
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ image: dataURL })
     })
     .then(response => response.json())
     .then(data => {
@@ -91,17 +149,23 @@ const GameBoard = () => {
       alert('まず将棋盤をキャプチャしてください。');
       return;
     }
-  
+
     const appUrl = "https://shogikaisetukun.com/";
     const twitterText = `${gameState.response.slice(0, 100)}... ${appUrl} ${imageURL}`;
-  
+
     const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(twitterText)}`;
     window.open(twitterUrl, '_blank');
   };
 
   return (
     <div>
-      <shogi-player-wc ref={shogiRef} sp_mode="play" sp_controller="true" sp_illegal_cancel="true"></shogi-player-wc>
+      <input type="file" onChange={handleFileChange} />
+      <shogi-player-wc
+        ref={shogiRef}
+        sp_controller="true" // コントローラを常に表示
+        sp_illegal_cancel="true"
+        sp_slider={spMode !== "play"} // 再生モードのみスライダーを表示
+      ></shogi-player-wc>
       <div className="explanation-container">
         <button className="explain-button" onClick={handleExplain} disabled={isLoading}>{isLoading ? '検討中...' : '解説'}</button>
         <button className="capture-button" onClick={captureShogiBoard}>キャプチャ</button>
